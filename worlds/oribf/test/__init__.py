b921bc9f0fa55df6d6f86abb09df665a6af0f8ab
MysroHub <sarahrobert238@gmail.com>
2025-02-25 19:36:18 -0500

    Create the base of the test structure and start a test

---
+++ worlds/oribf/test/__init__.py
@@ -0,0 +1,6 @@
+﻿from test.bases import WorldTestBase
+
+
+class OriBlindForestTestBase(WorldTestBase):
+    """Base class for Ori and the Blind Forest unit tests"""
+    game = "Ori and the Blind Forest"
---
+++ worlds/oribf/test/test.py
@@ -0,0 +1,19 @@
+﻿from worlds.oribf.test import OriBlindForestTestBase
+
+
+class Test(OriBlindForestTestBase):
+
+    def test_ok_ok_location(self) -> None:
+        locations = [
+            "LeftGrottoTeleporterExp",
+            "MistyKeystone3",
+            "MistyKeystone4",
+            "MistyGrenade",
+            "MistyPostClimbAboveSpikePit",
+            "LostGroveHiddenExp",
+            "LostGroveAbilityCell",
+            "LostGroveTeleporter"
+        ]
+
+        items = [["DoubleJump"]]
+        self.assertAccessDependency(locations, items)
