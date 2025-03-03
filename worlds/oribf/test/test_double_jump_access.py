e4b0339abcd665b799c9c418950f980d2945b1d6
MysroHub <sarahrobert238@gmail.com>
2025-03-03 11:37:35 -0500

    Rename the class test

---
+++ worlds/oribf/test/test_double_jump_access.py
@@ -0,0 +1,35 @@
+﻿from worlds.oribf.test import OriBlindForestTestBase
+
+
+class Test(OriBlindForestTestBase):
+
+    def test_double_jump_location(self) -> None:
+        locations = [
+            "LeftGrottoTeleporterExp",
+            "MistyKeystone3",
+            "MistyKeystone4",
+            "MistyGrenade",
+            "MistyPostClimbAboveSpikePit",
+            "MistyPostClimbSpikeCave",
+            "MistyFrogNookExp",
+            "MistyKeystone2",
+            "MistyAbilityCell",
+            "LostGroveHiddenExp",
+            "LostGroveAbilityCell",
+            "LostGroveTeleporter",
+            "GroveAboveSpiderWaterExp",
+            "GroveAboveSpiderWaterEnergyCell",
+            "UpperGinsoUpperLeftKeystone",
+            "UpperGinsoUpperRightKeystone",
+            "GinsoEscapeExit",
+            "GinsoEscapeHangingExp",
+            "GinsoEscapeProjectileExp",
+            "GinsoEscapeJumpPadExp",
+            "GinsoEscapeSpiderExp",
+            "GumonSeal",
+            "SunstonePlant",
+            "Sunstone"
+        ]
+
+        items = [["DoubleJump"]]
+        self.assertAccessDependency(locations, items)
